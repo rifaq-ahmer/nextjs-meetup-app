5f2f8b3f09f204477cc2f979a137253cc25da5fe
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2022-02-18 15:28:22 +0530

    added-meetup-list

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+
+const NewMeetupPage = () => {
+	return (
+    <>
+    <NewMeetupForm />
+    </>
+  )
+};
+
+export default NewMeetupPage;
