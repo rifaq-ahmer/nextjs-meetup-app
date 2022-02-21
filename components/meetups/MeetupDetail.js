69d230b659930ba3d74e0a1297fbf2a912be70c8
fayez-baig <coderninja16@gmail.com>
2022-02-21 18:42:30 +0530

    image optimization

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Image from "next/image";
 import classes from "./MeetupDetail.module.css";
 
 const MeetupDetail = ({ image, title, address, description }) => {
-	return (
-		<section className={classes.detail}>
-			<img src={image} alt={title} />
-			<h1>{title}</h1>
-			<address>{address}</address>
-			<p>{description}</p>
-		</section>
-	);
+  return (
+    <section className={classes.detail}>
+      <Image src={image} alt={title} height={20} width={20} />
+      <h1>{title}</h1>
+      <address>{address}</address>
+      <p>{description}</p>
+    </section>
+  );
 };
 
 export default MeetupDetail;
